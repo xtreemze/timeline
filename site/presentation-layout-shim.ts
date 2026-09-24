3d47d9ada48d68bf876c69c02091963e3a3bb2ae
Carlos Velasco <xtreemze@gmail.com>
2026-09-24 18:53:33 +0800

    docs: remove stale IIFE compatibility wording

--- site/presentation-layout-shim.ts
+++ site/presentation-layout-shim.ts
@@ -1,6 +1,6 @@
 /**
  * Temporary compatibility shim for presentation-layout ESM migration
- * Sets TimelinePresentationLayout on globalThis for backward compatibility with IIFE code
+ * Sets TimelinePresentationLayout on globalThis for legacy global consumers
  */
 
 import { TimelinePresentationLayout } from './presentation-layout.ts';
