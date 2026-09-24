3464ebcc8b98720c4fc47738e2ba4dfbaf2e83bb
Carlos Velasco <xtreemze@gmail.com>
2026-09-24 18:53:45 +0800

    docs: remove stale IIFE compatibility wording

--- site/temporal-graph-view-shim.ts
+++ site/temporal-graph-view-shim.ts
@@ -1,6 +1,6 @@
 /**
  * Temporary compatibility shim for temporal-graph-view ESM migration
- * Sets TemporalGraphView on globalThis for backward compatibility with IIFE code
+ * Sets TemporalGraphView on globalThis for legacy global consumers
  */
 
 import { TemporalGraphView } from './temporal-graph-view.ts';
