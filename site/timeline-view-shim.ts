3779ff50d578500b0d6010f54bd2985709b15422
Carlos Velasco <xtreemze@gmail.com>
2026-09-24 19:57:24 +0800

    style: organize timeline shim

--- site/timeline-view-shim.ts
+++ site/timeline-view-shim.ts
@@ -1,4 +1,6 @@
 import "./components/timeline-element.ts";
-import { TimelineView } from './timeline-view.ts';
+import { TimelineView } from "./timeline-view.ts";
+
 globalThis.TimelineView = TimelineView;
+
 export {};
