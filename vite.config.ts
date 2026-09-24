d86fd71139aa60df8a68a26beb1c0c827d5531fa
Carlos Velasco <xtreemze@gmail.com>
2026-09-24 21:10:46 +0800

    build: split heavyweight Vite runtime chunks

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,35 @@ export default defineConfig({
     outDir: "../dist",
     sourcemap: false,
     target: "chrome155",
+    manifest: true,
     rolldownOptions: {
       input: {
         main: new URL("./site/index.html", import.meta.url).pathname,
+      },
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: "pdf-runtime",
+              test: /node_modules[\\/]pdfjs-dist[\\/]/,
+              maxSize: 400_000,
+              priority: 30,
+            },
+            {
+              name: "world-rendering",
+              test:
+                /node_modules[\\/](?:@deck\\.gl|@luma\\.gl|@math\\.gl|@loaders\\.gl|@probe\\.gl)[\\/]/,
+              maxSize: 400_000,
+              priority: 25,
+            },
+            {
+              name: "legacy-graph",
+              test: /node_modules[\\/]@memgraph[\\/]orb[\\/]/,
+              maxSize: 400_000,
+              priority: 20,
+            },
+          ],
+        },
       },
     },
   },
