056a0c1e0184734fda2ffd8cd819c343183883f8
Carlos Velasco <xtreemze@gmail.com>
2026-09-24 19:00:45 +0800

    chore: keep build migration focused

--- site/timeline-navigation-shim.ts
+++ site/timeline-navigation-shim.ts
@@ -1,6 +1,6 @@
 /**
  * Temporary compatibility shim for timeline-navigation ESM migration
- * Sets TimelineNavigation on globalThis for legacy global consumers
+ * Sets TimelineNavigation on globalThis for backward compatibility with IIFE code
  */
 
 import { TimelineNavigation } from './timeline-navigation.ts';
