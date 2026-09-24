c949d9b6fd12b95918818f8d4cd2a62ab8c62cb1
Carlos Velasco <xtreemze@gmail.com>
2026-09-24 18:56:57 +0800

    fix: carry parent camera ordering into semantic branch

--- site/world/deck-world-surface.ts
+++ site/world/deck-world-surface.ts
@@ -1619,8 +1619,8 @@ export class DeckWorldSurface implements WorldSurface {
     this.#cameraOwned = true;
     this.#camera = createWorldCameraState(camera);
     this.#syncSpatialMode();
-    this.#deck.setProps({ viewState: this.#camera });
     this.#reclusterIfZoomCrossedThreshold();
+    this.#deck.setProps({ viewState: this.#camera });
   }
 
   focusEntity(id: EntityId): void {
