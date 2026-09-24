f8a4145239a3ce77d5dd3911512de07af551f5e0
Carlos Velasco <xtreemze@gmail.com>
2026-09-24 18:59:42 +0800

    chore: keep build migration focused

--- site/event-presentation-shim.ts
+++ site/event-presentation-shim.ts
@@ -1,6 +1,6 @@
 /**
  * Temporary compatibility shim for event-presentation ESM migration
- * Sets TimelinePresentation on globalThis for legacy global consumers
+ * Sets TimelinePresentation on globalThis for backward compatibility with IIFE code
  */
 
 import { TimelinePresentation } from './event-presentation.ts';
