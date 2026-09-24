cc85aa29c1b9249d63aa36dbd2ec8620f6661c34
Carlos Velasco <xtreemze@gmail.com>
2026-09-24 19:00:02 +0800

    chore: keep build migration focused

--- site/location-map-shim.ts
+++ site/location-map-shim.ts
@@ -1,6 +1,6 @@
 /**
  * Temporary compatibility shim for location-map ESM migration
- * Sets TimelineLocationMap on globalThis for legacy global consumers
+ * Sets TimelineLocationMap on globalThis for backward compatibility with IIFE code
  */
 
 import { TimelineLocationMap } from './location-map.ts';
