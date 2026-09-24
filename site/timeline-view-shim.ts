3adc84f8d60160dd11fa9c8f35c4804b2ed67b0d
Carlos Velasco <xtreemze@gmail.com>
2026-09-24 20:02:33 +0800

    style: format timeline component shim

--- site/timeline-view-shim.ts
+++ site/timeline-view-shim.ts
@@ -1,4 +1,6 @@
 import "./components/luum-timeline.ts";
-import { TimelineView } from './timeline-view.ts';
+import { TimelineView } from "./timeline-view.ts";
+
 globalThis.TimelineView = TimelineView;
+
 export {};
