1862a9f58c7b8552f19e26209de6fe79b59f5eeb
Carlos Velasco <xtreemze@gmail.com>
2026-09-24 19:00:09 +0800

    chore: keep build migration focused

--- site/presentation-layout-shim.ts
+++ site/presentation-layout-shim.ts
@@ -1,6 +1,6 @@
 /**
  * Temporary compatibility shim for presentation-layout ESM migration
- * Sets TimelinePresentationLayout on globalThis for legacy global consumers
+ * Sets TimelinePresentationLayout on globalThis for backward compatibility with IIFE code
  */
 
 import { TimelinePresentationLayout } from './presentation-layout.ts';
